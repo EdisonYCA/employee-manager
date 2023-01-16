7a2e7a06b90aee1bfe79041fb69b317faf459a12
EdisonYCA <edisonyca127@gmail.com>
2023-01-16 13:09:24 -0500

    reset code

--- AdminUI.cpp
+++ AdminUI.cpp
@@ -1,37 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int loginMenu() {
-	int response;
-	cout << "Select a valid menu option."<<endl;
-	cout << "1. Sign Up\n2. Sign In\n-> ";
-	cin >> response;
-
-	return response;
-}
-
-void signUp() {
-	int id = 123;
-		//generateID();
-	string first_name, last_name;
-	double pay_rate;
-
-	cout << "Enter employee first name: ";
-	cin >> first_name;
-
-	cout << "Enter employee last name: ";
-	cin >> last_name;
-
-	cout << "Enter employee pay_rate: ";
-	cin >> pay_rate;
-
-
-}
-
 int main() {
-	if (loginMenu() == 1) {
-		signUp();
-	}
-
+	cout << "hello world";
 	return 0;
 }
