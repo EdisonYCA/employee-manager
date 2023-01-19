216b962d8af0518019059e173a09b51c3ed63c9e
EdisonYCA <edisonyca127@gmail.com>
2023-01-18 20:50:57 -0500

    connected sqlite database to project. Created method to create punches database, create punches table, insert data, and view database data.

--- DatabaseHandler.cpp
+++ DatabaseHandler.cpp
@@ -1,20 +1,112 @@
 #include <iostream>
-#include "C:/Users/Ediso/Desktop/ROAD TO INTERNSHIP/C++/SQLAPI/include/SQLAPI.h"
+#include "sqlite/sqlite3.h"
 using namespace std;
 
-int main() {
-	SAConnection conn;
-	try {
-		conn.Connect(_TSA("test1"), _TSA("root"), _TSA("Ma3a6euv0!"), SA_MySQL_Client);
-		cout << "We are connected!\n";
+void createDB(const char* databaseName); // create a database
+void createPunchTable(const char* databaseName); // create table to record punch ins
+void viewDatabase(const char* databaseName); // show all records in database
+static int callback(void* NotUsed, int agrc, char** argv, char** azColName);
+void insertData(const char* databaseName); // insert data into database
 
-		conn.Disconnect();
-		cout << "we are disconnected!\n";
-	}
-	catch (SAException& e) {
-		conn.Rollback();
-		cout << "\n" << e.ErrText().GetMultiByteChars();
-	}
+int main(int argc, char* argv[])
 
-	return 0;
+{
+    createDB("PunchesDB");
+    createPunchTable("PunchesDB");
+    insertData("PunchesDB");
+    viewDatabase("PunchesDB");
+
+    return 0;
+
 }
+
+void createDB(const char* databaseName) {
+    sqlite3* db;
+
+    sqlite3_open( // create database if DNE
+        databaseName,
+        &db
+    );
+
+    sqlite3_close(db);
+}
+
+void createPunchTable(const char* databaseName) {
+    sqlite3 *db;
+
+    string query = "CREATE TABLE IF NOT EXISTS PUNCHES("
+        "Id INT PRIMARY KEY, "
+        "PunchDate DATE, " 
+        "InTime TIME, " 
+        "OutTime TIME, " 
+        "Hours FLOAT, "
+        "PayPeriodStart DATE, "
+        "PayPeriodEnd DATE);";
+
+    sqlite3_open(databaseName, &db);
+
+    char* err;
+    int result = sqlite3_exec(db,
+    query.c_str(),
+        NULL,
+        NULL,
+        &err);
+
+   if (result != SQLITE_OK) {
+        cout << "Error while create punch table: " << err << endl;
+   }
+   else {
+       cout << "Table created succesfully!" << endl;
+   }
+        
+   sqlite3_close(db);
+}
+
+void viewDatabase(const char* databaseName) {
+    
+    sqlite3* db;
+
+    string query = "SELECT * FROM PUNCHES";
+
+
+    sqlite3_open(databaseName, &db);
+    
+    char* err;
+    int result = sqlite3_exec(db, query.c_str(), callback, NULL, &err);
+    
+    if (result != SQLITE_OK) {
+        cout << "Error selecting data: " << err << endl;
+    }
+    sqlite3_close(db);
+}
+
+static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++) {
+        cout << azColName[i] << ": " << argv[i] << endl;
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
+void insertData(const char* databaseName) {
+    sqlite3* db;
+
+    sqlite3_open(databaseName, &db);
+
+    string query = "INSERT INTO PUNCHES VALUES(123, '2015-12-17', '13:20', '13:20', 9, '2016-12-17', '2017-12-17');";
+
+    char* err;
+    int result = sqlite3_exec(db, query.c_str(), NULL, 0, &err);
+
+    if (result != SQLITE_OK) {
+        cout << "Error inserting data: " << err << endl;
+    }
+    else {
+        cout << "Data inserted!" << endl;
+    }
+}
+
+
+
