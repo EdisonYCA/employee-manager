abb1671b5f81bd746b4c5e09faf8028e224c050c
EdisonYCA <edisonyca127@gmail.com>
2023-01-25 20:37:36 -0500

    Created method to search for an employee using and Id and password and return true if found. Created methods to update first name, last name, password, id, and pay rate for a user.

--- DatabaseHandler.cpp
+++ DatabaseHandler.cpp
@@ -12,13 +12,20 @@ static int callback(void* NotUsed, int agrc, char** argv, char** azColName);
 // void insertData(const char* databaseName); // insert data into database
 void insertEmployee(const char* databaseName, int employeeID, const char* firstName, const char* lastName, const char* password);
 void insertDummyData(const char* databaseName);
-void searchForEmployee(const char* databaseName, int employeeID, const char* password);
+bool searchForEmployee(const char* databaseName, int employeeID, const char* password);
+void setFirstName(const char* databaseName, int id, const char* password, const char* newFirstName);
+void setLastName(const char* databaseName, int id, const char* password, const char* newLastName);
+void setPayRate(const char* databaseName, int id, const char* password, float newPayRate);
+void setId(const char* databaseName, int id, const char* password, int newId);
+void setPassword(const char* databaseName, int id, const char* password, const char* newPassword);
 
+/*
 int main() {
     initalizeDatabase();
-    viewDatabase("PunchesDB.db");
+    searchForEmployee("PunchesDB.db", 12345, "Ma3a6uev0!");
     return 0;
 }
+*/
 
 void initalizeDatabase()
 {
@@ -158,22 +165,115 @@ void insertDummyData(const char* databaseName) {
     }
 }
 
-void searchForEmployee(const char* databaseName, int employeeID, const char* password) {
+bool searchForEmployee(const char* databaseName, int employeeID, const char* password) {
     sqlite3* db;
 
-    string query = "SELECT Id, FirstName, LastName, PayRate FROM EMPLOYEES WHERE Id = " +
+    string query = "SELECT Id, FirstName, LastName, PayRate, Password FROM EMPLOYEES WHERE Id = " +
         to_string(employeeID) + " AND Password = " + "\"" + string(password) + "\"";
 
     sqlite3_open(databaseName, &db);
 
     char* err;
+    cout << "Retrieving employee information..." << endl;
     int result = sqlite3_exec(db, query.c_str(), callback, NULL, &err);
 
     if (result != SQLITE_OK) {
-        cout << "Error selecting data: " << err << endl;
+        cout << "Error: " << err;
+        return false;
     }
 
     sqlite3_close(db);
+    return true;
+}
+
+void setFirstName(const char* databaseName, int id, const char *password, const char *newFirstName) {
+    sqlite3* db;
+
+    string query = string("UPDATE EMPLOYEES SET FirstName = ") + "\"" + string(newFirstName) + "\"" + " WHERE Id = " 
+        + to_string(id) + " AND " + "Password = " + "\"" + string(password) + "\"";
+
+    sqlite3_open(databaseName, &db);
+    char* err;
+    int result = sqlite3_exec(db, query.c_str(), NULL, 0, &err);
+
+    if (result != SQLITE_OK) {
+        cout << "Error setting first name: " << err << endl;
+    }
+    else {
+        cout << "Updated first name succesfully!" << endl;
+    }
+}
+
+void setLastName(const char* databaseName, int id, const char* password, const char* newLastName) {
+    sqlite3* db;
+
+    string query = string("UPDATE EMPLOYEES SET LastName = ") + "\"" + string(newLastName) + "\"" + " WHERE Id = "
+        + to_string(id) + " AND " + "Password = " + "\"" + string(password) + "\"";
+
+    sqlite3_open(databaseName, &db);
+    char* err;
+    int result = sqlite3_exec(db, query.c_str(), NULL, 0, &err);
+
+    if (result != SQLITE_OK) {
+        cout << "Error setting last name: " << err << endl;
+    }
+    else {
+        cout << "Updated last name succesfully!" << endl;
+    }
+}
+
+void setPayRate(const char* databaseName, int id, const char* password, float newPayRate) {
+    sqlite3* db;
+
+    string query = string("UPDATE EMPLOYEES SET PayRate = ") + "\"" + to_string(newPayRate) + "\"" + " WHERE Id = "
+        + to_string(id) + " AND " + "Password = " + "\"" + string(password) + "\"";
+
+    sqlite3_open(databaseName, &db);
+    char* err;
+    int result = sqlite3_exec(db, query.c_str(), NULL, 0, &err);
+
+    if (result != SQLITE_OK) {
+        cout << "Error setting pay rate: " << err << endl;
+    }
+    else {
+        cout << "Updated pay rate succesfully!" << endl;
+    }
+}
+
+void setId(const char* databaseName, int id, const char* password, int newId) {
+    sqlite3* db;
+
+    string query = string("UPDATE EMPLOYEES SET Id = ") + "\"" + to_string(newId) + "\"" + " WHERE Id = "
+        + to_string(id) + " AND " + "Password = " + "\"" + string(password) + "\"";
+
+    sqlite3_open(databaseName, &db);
+    char* err;
+    int result = sqlite3_exec(db, query.c_str(), NULL, 0, &err);
+
+    if (result != SQLITE_OK) {
+        cout << "Error setting id: " << err << endl;
+    }
+    else {
+        cout << "Updated id succesfully!" << endl;
+    }
+}
+
+void setPassword(const char* databaseName, int id, const char* password, const char* newPassword) {
+    sqlite3* db;
+
+    string query = string("UPDATE EMPLOYEES SET Password = ") + "\"" + string(newPassword) + "\"" + " WHERE Id = "
+        + to_string(id) + " AND " + "Password = " + "\"" + string(password) + "\"";
+
+    sqlite3_open(databaseName, &db);
+    char* err;
+    int result = sqlite3_exec(db, query.c_str(), NULL, 0, &err);
+
+    if (result != SQLITE_OK) {
+        cout << "Error setting password: " << err << endl;
+    }
+    else {
+        cout << "Updated password succesfully!" << endl;
+    }
 }
 
 
